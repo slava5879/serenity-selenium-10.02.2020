12ffc50894825f811012ba76b868cfed92144409
viacheslav <lan1457@ukr.net>
2020-02-27 07:58:56 +0100

    Homework 4 commit

--- src/test/java/tests/SearchTest.java
+++ src/test/java/tests/SearchTest.java
@@ -1,12 +1,31 @@
 package tests;
 
+import net.serenitybdd.junit.runners.SerenityParameterizedRunner;
 import net.serenitybdd.junit.runners.SerenityRunner;
+import net.thucydides.junit.annotations.TestData;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
-@RunWith(SerenityRunner.class)
+import java.util.Arrays;
+import java.util.Collection;
+
+@RunWith(SerenityParameterizedRunner.class)
 public class SearchTest extends BaseTest {
+    private String searchTerm;
+
+    public SearchTest(String searchTerm) {
+        this.searchTerm = searchTerm;
+    }
+
+    @TestData
+    public static Collection<Object[]> testData(){
+        return Arrays.asList(new Object[][]{
+                {"hr"},
+                {"HR"},
+                {"Human resources"}
+        });
+    }
 
     @Before
     public void before() {
@@ -20,10 +39,10 @@ public class SearchTest extends BaseTest {
         user
                 .validatePageTitle("LinkedIn")
                 .homePage()
-                .searchFor("hr");
+                .searchFor(searchTerm);
         user
                 .validatePageTitle("LinkedIn")
                 .searchPage()
-                .verifyEachResultContains("hr");
+                .verifyEachResultContains(searchTerm);
     }
 }
